3a4ed94cdecd619a49ff0eecf4a81bd5ee31289a
AfterW0rld <46646338+AfterW0rld@users.noreply.github.com>
2019-02-27 21:06:01 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,35 +6,35 @@ public class Main {
 
 
 
-//________________________________
-//    public static void main(String[] args) {
-//        double a;
-//        int b;
-//        Scanner in = new
-//                Scanner(System.in);
-//        System.out.print("Введите число - ");
-//        a = in.nextDouble();
-//        System.out.print("Выберите функцию:  " +
-//                "1 - найти корень числа; " +
-//                "2 - возвести число в степень: ");
-//        b = in.nextInt();
-//        switch (b) {
-//            case 1:
-//                double c = Math.sqrt(a);
-//                System.out.println("Корень из числа " + a + " = " + c);
-//                break;
-//            case 2:
-//                System.out.print("В какую степень возвести число: ");
-//                int s = in.nextInt();
-//                double n = Math.pow(a, s);
-//                System.out.println("Число " + a + " в " + s + " степени = " + n);
-//                break;
-//            default:
-//                System.out.println("Ошибка!");
-//                break;
-//
-//
-//        }
-//
-//    }
+
+    public static void main(String[] args) {
+        double a;
+        int b;
+        Scanner in = new
+                Scanner(System.in);
+        System.out.print("Введите число - ");
+        a = in.nextDouble();
+        System.out.print("Выберите функцию:  " +
+                "1 - найти корень числа; " +
+                "2 - возвести число в степень: ");
+        b = in.nextInt();
+        switch (b) {
+            case 1:
+               double c = Math.sqrt(a);
+               System.out.println("Корень из числа " + a + " = " + c);
+               break;
+           case 2:
+               System.out.print("В какую степень возвести число: ");
+               int s = in.nextInt();
+               double n = Math.pow(a, s);
+               System.out.println("Число " + a + " в " + s + " степени = " + n);
+               break;
+           default:
+               System.out.println("Ошибка!");
+               break;
+
+
+       }
+
+   }
 }
